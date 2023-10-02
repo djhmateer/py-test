50fd89a27b3741796443fbcb6e1a5f9983901ab2
Dave Mateer <davemateer@gmail.com>
2023-10-02 15:08:54 +0100

    auto

---
+++ list.py
@@ -0,0 +1,13 @@
+
+# original code
+names = ['Corey', 'Chris', 'Travis', 'Dave']
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
+
+# 0 Corey
+# 1 Chris
+# 2 Travis
+# 3 Dave
